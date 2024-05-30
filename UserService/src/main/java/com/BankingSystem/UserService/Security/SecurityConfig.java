99d1fd30a4c0ae69552ecfc37ae1e43a0cce14ce
Anant <anantsinghraghuvanshi1@gmail.com>
2024-05-31 00:14:48 +0530

    basic files added- user service

---
+++ UserService/src/main/java/com/BankingSystem/UserService/Security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.BankingSystem.UserService.Security;
+
+import com.BankingSystem.UserService.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(username -> userService.findByUsername(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found")))
+                .passwordEncoder(passwordEncoder());
+    }
+}
--- UserService/src/main/java/com/BankingSystem/UserService/Service/UserService.java
+++ UserService/src/main/java/com/BankingSystem/UserService/Service/UserService.java
@@ -3,14 +3,24 @@ package com.BankingSystem.UserService.Service;
 import com.BankingSystem.UserService.Entity.User;
 import com.BankingSystem.UserService.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User registerUser(User user){
-        user.setPassword(user.getPassword());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String userName){
+        return userRepository.findByUsername(userName);
     }
 }
