f64cd6c995dbab5d4f908c10cbc68d97a2d9803a
Anant <anantsinghraghuvanshi1@gmail.com>
2024-05-31 10:59:29 +0530

    Generation type update

--- UserService/src/main/java/com/BankingSystem/UserService/Entity/User.java
+++ UserService/src/main/java/com/BankingSystem/UserService/Entity/User.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
     private String password;
