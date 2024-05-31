7c518a64488fd2f605055236222c3ad2688af3a7
Anant <anantsinghraghuvanshi1@gmail.com>
2024-05-31 10:33:29 +0530

    Account Service done - account service

---
+++ AccountService/src/main/java/com/BankingSystem/AccountService/Service/AccountService.java
@@ -0,0 +1,27 @@
+package com.BankingSystem.AccountService.Service;
+
+import com.BankingSystem.AccountService.Entity.Account;
+import com.BankingSystem.AccountService.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account createAccount(Account account){
+        return accountRepository.save(account);
+    }
+
+    public List<Account> getAccountsById(Long userId){
+        return accountRepository.findByUserId(userId);
+    }
+
+    public Account getAccountById(Long accountId){
+        return accountRepository.findById(accountId).orElse(null);
+    }
+}
