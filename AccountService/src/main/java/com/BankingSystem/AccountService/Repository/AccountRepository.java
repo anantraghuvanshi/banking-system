9a8edda8c17f0612d2ad654dd2d98fce245ece11
Anant <anantsinghraghuvanshi1@gmail.com>
2024-05-31 10:33:19 +0530

    Account Repo done - account service

---
+++ AccountService/src/main/java/com/BankingSystem/AccountService/Repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.BankingSystem.AccountService.Repository;
+
+import com.BankingSystem.AccountService.Entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUserId(Long userId);
+}
